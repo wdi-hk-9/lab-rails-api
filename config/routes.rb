d1964a1502945dae8076abd410d1cfa998dbeac4
“Ian <ianyhkwok@hotmail.com>
2015-11-09 17:51:11 +0800

    Movies Controller Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'movies#index'
 
+  namespace :api do
+    resources :movies, only:[:index, :show, :create, :update, :destroy], :defaults => {:format => 'json'}
+  end
+
   resources :movies
   resources :actors
 end
