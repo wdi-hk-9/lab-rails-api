01f57960263ceb0462816b7ab007f209e6115b3a
Bryan Fong <bryan.fch@gmail.com>
2015-11-09 23:24:07 +0800

    done

---
+++ app/controllers/api/movies_controller.rb
@@ -0,0 +1,41 @@
+module API
+  class MoviesController < ApplicationController
+    def index
+      render json: Movie.all
+    end
+
+    def show
+      render json: Movie.find(params[:id])
+    end
+
+    def create
+      movie = Movie.new(movie_params)
+      if movie.save
+        render json: movie, status: 201, location: [:api, movie]
+      else
+        render json: movie.errors, status: 403, location: [:api, movie]
+      end
+    end
+
+    def update
+      movie = Movie.find(params[:id])
+      if movie.update(movie_params)
+        head 204
+      else
+        render json: movie.errors, status: 403, location: [:api, movie]
+      end
+    end
+
+    def destroy
+      movie = Movie.find(params[:id])
+      movie.destroy
+      head 204
+    end
+
+    private
+    def movie_params
+      params.require(:movie).permit(:title, :summary, :youtube_embed_id, :thumbnail)
+    end
+
+  end
+end
