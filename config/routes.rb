0f0388848bc014931c782d7c0f012969fdfcbcdb
“Ian <ianyhkwok@hotmail.com>
2015-11-10 10:20:55 +0800

    Updated Syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   namespace :api do
-    resources :movies, only:[:index, :show, :create, :update, :destroy], :defaults => {:format => 'json'}
+    resources :movies, :actors, only:[:index, :show, :create, :update, :destroy], defaults: {:format => 'json'}
   end
 
-   namespace :api do
-    resources :actors, only:[:index, :show, :create, :update, :destroy], :defaults => {:format => 'json'}
-  end
+  #  namespace :api do
+  #   resources :actors, only:[:index, :show, :create, :update, :destroy], :defaults => {:format => 'json'}
+  # end
 
-  resources :movies
-  resources :actors
+  resources :movies, :actors
+
 end
